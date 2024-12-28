68bef57e46a7d3b2b32fe52a77691335cb245f49
vzeme <v.zemela@gmail.com>
2024-12-28 12:53:39 +0200

    Implementing Step specific Definitions

--- src/test/java/stepDefinitions/Contact_Us_Steps.java
+++ src/test/java/stepDefinitions/Contact_Us_Steps.java
@@ -66,6 +66,26 @@ public class Contact_Us_Steps {
         driver.findElement(By.xpath("//textarea[@name=\"message\"]")).sendKeys("Hello world " + generateRandomString(20));
     }
 
+    @When("I enter a specific first name {word}")
+    public void i_enter_a_specific_first_name(String firstName) {
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys(firstName);
+    }
+
+    @When("I enter a specific last name {word}")
+    public void i_enter_a_specific_last_name(String lastName) {
+        driver.findElement(By.xpath("//input[@name=\"last_name\"]")).sendKeys(lastName);
+    }
+
+    @When("I enter a specific email address {word}")
+    public void i_enter_a_specific_email_address(String email) {
+        driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys(email);
+    }
+
+    @When("I enter a specific comment {string}")
+    public void i_enter_a_specific_comment(String comment) {
+        driver.findElement(By.xpath("//textarea[@name=\"message\"]")).sendKeys(comment);
+    }
+
 
     @And("I click on the submit button")
     public void i_click_on_the_submit_button() {
